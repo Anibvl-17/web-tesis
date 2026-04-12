3dce72f95a1c9478874dc812929077205d56554d
Anibvl-17 <claudio.candia2301@alumnos.ubiobio.cl>
2026-04-12 14:42:06 -0400

    agrega componente de enlaces

---
+++ src/components/AnchorLink.jsx
@@ -0,0 +1,12 @@
+function AnchorLink({ href, text }) {
+  return (
+    <a
+      href={href}
+      className="active:text-red-500/90 transition-all duration-200 hover:text-mist-200 hover:cursor-pointer"
+    >
+      {text}
+    </a>
+  );
+}
+
+export default AnchorLink;
