60d2a4c4199780da98dc85b1e2d928910ea8b6d6
Anibvl-17 <claudio.candia2301@alumnos.ubiobio.cl>
2026-04-13 00:19:51 -0400

    agrega atributo target

--- src/components/AnchorLink.jsx
+++ src/components/AnchorLink.jsx
@@ -1,8 +1,9 @@
-function AnchorLink({ href, text }) {
+function AnchorLink({ href, text, target }) {
   return (
     <a
       href={href}
       className="active:text-red-500/90 transition-all duration-200 hover:text-mist-200 hover:cursor-pointer"
+      target={target}
     >
       {text}
     </a>
