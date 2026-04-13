1b48051938fb4ce133e3b4ae7dfe4ac93842af53
Anibvl-17 <claudio.candia2301@alumnos.ubiobio.cl>
2026-04-13 00:21:37 -0400

    agrega atributo target

--- src/components/AnchorLink.jsx
+++ src/components/AnchorLink.jsx
@@ -1,11 +1,14 @@
-function AnchorLink({ href, text, target }) {
+import { FaExternalLinkAlt } from "react-icons/fa";
+
+function AnchorLink({ href, text, target, external }) {
   return (
     <a
       href={href}
-      className="active:text-red-500/90 transition-all duration-200 hover:text-mist-200 hover:cursor-pointer"
+      className="flex flex-row gap-2 items-center jactive:text-red-500/90 transition-all duration-200 hover:text-mist-200 hover:cursor-pointer"
       target={target}
     >
       {text}
+      {external && <FaExternalLinkAlt className="h-3 w-3"/>}
     </a>
   );
 }
